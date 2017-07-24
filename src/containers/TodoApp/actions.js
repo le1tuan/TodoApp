e27123ff72569d045bee819a97e7a1d3d877c91b
le1tuan <lekoi9x@gmail.com>
2017-07-24 16:59:17 +0700

    Create Header

--- src/containers/TodoApp/actions.js
+++ src/containers/TodoApp/actions.js
@@ -10,7 +10,6 @@ export const showAll = () => {
     return {
         type: 'SHOW_ALL',
     }
-<<<<<<< 21e975da869ea60ab00763cd2d869011ee14b581
 }
 
 export const toggle = (id) => {
@@ -18,6 +17,4 @@ export const toggle = (id) => {
         type: 'TOGGLE_TODO',
         id: id
     }
-=======
->>>>>>> Refactor and Rewrite the new todo application
 }
--- src/containers/TodoApp/reducers.js
+++ src/containers/TodoApp/reducers.js
@@ -4,12 +4,16 @@ export const todos = (state = [], action) => {
         case 'ADD_TODO':
             return [
                 ...state,
+<<<<<<< 02a24cf6c0098130f2bc09489a3f41fbcbaabe86
 <<<<<<< 21e975da869ea60ab00763cd2d869011ee14b581
+=======
+>>>>>>> Create Header
                 {
                     text: action.text,
                     id: action.id,
                     completed: false
                 }
+<<<<<<< 02a24cf6c0098130f2bc09489a3f41fbcbaabe86
             ];
         case 'TOGGLE_TODO':
             return state.map(x => {
@@ -22,6 +26,16 @@ export const todos = (state = [], action) => {
                 action.text
             ];
 >>>>>>> Refactor and Rewrite the new todo application
+=======
+            ];
+        case 'TOGGLE_TODO':
+            return state.map(x => {
+                if(x.id === action.id){
+                    x.completed = !x.completed
+                }
+                return x
+            });
+>>>>>>> Create Header
         default: return state;
     }
 }
